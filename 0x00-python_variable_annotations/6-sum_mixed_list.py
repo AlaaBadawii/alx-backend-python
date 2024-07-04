b5646f565b9f047ac47e6dfe490feebc9a3062a1
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-07-05 00:58:33 +0300

    check sum_mixed_list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Sum mixed list module"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Sum mixed list module"""
+    return float(sum(mxd_lst))
