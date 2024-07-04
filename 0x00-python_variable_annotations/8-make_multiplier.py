2e8b236f06ac4027484a7df384e6d38810f09c8c
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-07-05 01:06:59 +0300

    check

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Make a multiplier"""
+
+
+def make_multiplier(multiplier: float) -> callable:
+    """Make a multiplier"""
+    def multiply(n: float) -> float:
+        """Multiply a float by multiplier"""
+        return n * multiplier
+    return multiply
