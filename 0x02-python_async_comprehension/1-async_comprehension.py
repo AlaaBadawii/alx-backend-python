d139a2549998bb947476c03724dd94a28d2c1607
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-07-10 00:03:14 +0300

    check 1-async_comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,6 +6,7 @@ from importlib import import_module as using
 
 async_generator = using('0-async_generator').async_generator
 
+
 async def async_comprehension() -> List[float]:
     """Collects 10 random numbers using
     an async generator."""
