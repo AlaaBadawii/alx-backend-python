026fa0e8655dd70e087d88a6e8f4cd35d104ce3f
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-07-28 10:58:43 +0300

    check

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -62,24 +62,24 @@ class TestMemoize(unittest.TestCase):
     def test_memoize(self):
         """test_memoize method"""
         class TestClass:
+            """class TestClass"""
             def a_method(self):
+                """"""
                 return 42
 
             @memoize
             def a_property(self):
+                """"""
                 return self.a_method()
 
         with patch.object(TestClass, 'a_method', return_value=42) as mock_m:
             instance = TestClass()
-            # Call a_property twice
             result1 = instance.a_property
             result2 = instance.a_property
 
-            # Check the return value
             self.assertEqual(result1, 42)
             self.assertEqual(result2, 42)
 
-            # Ensure a_method is called only once
             mock_m.assert_called_once()
 
 
